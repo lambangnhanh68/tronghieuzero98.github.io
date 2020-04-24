983e181c46cf43e5ea0dd61107968a0acc1b1708
tronghieuzero98 <56430211+tronghieuzero98@users.noreply.github.com>
2020-04-24 14:04:52 +0700

    ok change all the files

---
+++ precache-manifest.383bbf9c674e61d3c0b629fb3a533ac6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3f25337f76714a6c657025a673e2df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "ffcd79471a439b730468",
+    "url": "/static/css/main.89f78581.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcd79471a439b730468",
+    "url": "/static/js/main.e2ec5b4d.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
